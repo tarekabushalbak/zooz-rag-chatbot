b609cb350fe0fa795cc4b9218dc58d45051f6832
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-12 13:56:58 +0300

    Download ChromaDB as zip

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown --folder https://drive.google.com/drive/folders/1KhzlZ3Zt909RswY7FKkn-rCjtmxMnCvz -O chroma_db
+gdown https://drive.google.com/uc?id=1At3L_TCZE0IY6XHKl1FGSyU1Hzetn3RQ -O chroma_db.zip
+unzip -o chroma_db.zip -d .
+rm chroma_db.zip
