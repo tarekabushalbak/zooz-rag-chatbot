1dd50af7b91876ed6e3c1b78536c34999d5f7817
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-12 13:30:34 +0300

    Fix gdown command

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown --folder https://drive.google.com/drive/folders/1KhzlZ3Zt909RswY7FKkn-rCjtmxMnCvz -O chroma_db --remaining-ok
+gdown --folder https://drive.google.com/drive/folders/1KhzlZ3Zt909RswY7FKkn-rCjtmxMnCvz -O chroma_db
