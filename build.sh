4d6aa90140c8a936fd156c4826fea17a952ae57c
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-14 18:09:08 +0300

    Rebuild ChromaDB without junk files, guard against fake contact info

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown https://drive.google.com/uc?id=1At3L_TCZE0IY6XHKl1FGSyU1Hzetn3RQ -O chroma_db.zip
+gdown https://drive.google.com/uc?id=13WJL9eSwx1wOpHK5A1xNMZv-4poIXjZo -O chroma_db.zip
 unzip -o chroma_db.zip -d .
 rm chroma_db.zip
