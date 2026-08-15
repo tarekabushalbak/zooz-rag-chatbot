3e59fe913f98a6ed04d650c9b075844fabc26f9a
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-15 19:50:06 +0300

    Update ChromaDB link to cleaned version v2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown https://drive.google.com/uc?id=13WJL9eSwx1wOpHK5A1xNMZv-4poIXjZo -O chroma_db.zip
+gdown https://drive.google.com/uc?id=1-J5T5_ruwcx87PVx7A124uVLddL7SI4w -O chroma_db.zip
 unzip -o chroma_db.zip -d .
 rm chroma_db.zip
