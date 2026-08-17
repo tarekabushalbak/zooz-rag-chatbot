bb93ad2b473f1792ad3c8ecea3c2acb3729077d7
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-17 16:16:39 +0300

    Update ChromaDB to v3 with fixed Hebrew encoding

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown https://drive.google.com/uc?id=1-J5T5_ruwcx87PVx7A124uVLddL7SI4w -O chroma_db.zip
+gdown https://drive.google.com/uc?id=1emBSTIYR_FGeHFTCaXOROWjmVUN4NfYb -O chroma_db.zip
 unzip -o chroma_db.zip -d .
 rm chroma_db.zip
