704bce6fd46a5d37925ddccb1154f59641b1bd1a
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-19 02:21:46 +0300

    Update ChromaDB to v5

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown https://drive.google.com/uc?id=1U1aQ9mlYxE0U1f6-fWcD7JUncXEwozn4 -O chroma_db.zip
+gdown https://drive.google.com/uc?id=1PFYRBHW41UJSNnz-fTkJsiPVDjOj4xQR -O chroma_db.zip
 unzip -o chroma_db.zip -d .
 rm chroma_db.zip
