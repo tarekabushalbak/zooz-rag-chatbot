75739491cda9a429d73ae234e2c72b30f9ad07ff
Tarek Abu Shalbak <abushalbaktarek968@gmail.com>
2026-08-19 00:00:38 +0300

    Update ChromaDB to v4 with email-protected filter fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install gdown
-gdown https://drive.google.com/uc?id=1emBSTIYR_FGeHFTCaXOROWjmVUN4NfYb -O chroma_db.zip
+gdown https://drive.google.com/uc?id=1U1aQ9mlYxE0U1f6-fWcD7JUncXEwozn4 -O chroma_db.zip
 unzip -o chroma_db.zip -d .
 rm chroma_db.zip
